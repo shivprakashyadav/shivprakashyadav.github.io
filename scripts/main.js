6dabaf99da53585e76678775350c268c222dc613
shivprakashyadav <shiv69rules@gmail.com>
2019-01-02 22:11:30 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -113,12 +113,4 @@ magicalUnderlines.forEach(function (underline) {
 });
 //# sourceMappingURL=main.js.map
 
-(function($){
-  'use strict';
-    $(window).on('load', function () {
-        if ($(".pre-loader").length > 0)
-        {
-            $(".pre-loader").fadeOut("slow");
-        }
-    });
-})(jQuery)
+
